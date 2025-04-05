d21b46af50e81998853c8dd87b1bb08ac6469a8f
JotaVPinheiro <jotavpinheiro@gmail.com>
2025-04-05 14:52:45 -0300

    feat: add jwt auth

--- tasking_back/urls.py
+++ tasking_back/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from tasking_back.tasking_back import views
 
@@ -12,4 +13,6 @@ router.register(r"groups", views.GroupViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
